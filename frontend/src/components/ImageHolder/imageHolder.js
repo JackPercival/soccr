1c16a762f83cf43c37b94b0aa42fad9866c9487e
Jack Percival <jackpercival7@gmail.com>
2021-10-11 10:13:20 -0700

    Add hover effects

--- frontend/src/components/ImageHolder/imageHolder.js
+++ frontend/src/components/ImageHolder/imageHolder.js
@@ -1,15 +1,29 @@
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './imageHolder.css'
 
 function ImageHolder({image}) {
     console.log("********", image.User)
+    const [hovered, setHovered] = useState('');
+
+    const toggleInfo = () => {
+        if (hovered === 'imageHovered') {
+            setHovered('')
+        } else {
+            setHovered('imageHovered')
+        }
+    }
+
     return (
         <NavLink to={`/images/${image.id}`}>
-            <div className="imageExploreContainer">
+            <div className="imageExploreContainer" id={hovered}
+                onMouseOver={toggleInfo}
+                onMouseOut={toggleInfo}>
                 <img className="imagesExplore" src={image.image_url} alt={image.title}/>
+                <p className="imageTitle">{image.title}</p>
+                <p className="imageAuthor">{image.User? image.User.username: null}</p>
             </div>
-        {image.title}
-        {/* {image.User.username} */}
         </NavLink>
     )
 }
