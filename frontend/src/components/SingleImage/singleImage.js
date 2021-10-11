1bbe71a44ad33a762e9e4741318d7501e4149591
Jack Percival <jackpercival7@gmail.com>
2021-10-11 16:56:23 -0700

    Add delete styling

--- frontend/src/components/SingleImage/singleImage.js
+++ frontend/src/components/SingleImage/singleImage.js
@@ -10,11 +10,15 @@ import './singleImage.css'
 
 function SingleImage() {
     const dispatch = useDispatch();
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [grow, setGrow] = useState('')
+    const sessionUser = useSelector(state => state.session.user);
 
     const { imageId } = useParams();
     const image = useSelector(state => state.images[imageId]);
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [grow, setGrow] = useState('')
+    const [showDelete, setShowDelete] = useState(false);
+
 
     useEffect(() => {
         dispatch(getAllImages()).then(() => setIsLoaded(true));
@@ -31,7 +35,7 @@ function SingleImage() {
     return (
         <div className="container">
             <Header />
-            <main>
+            <main className="mainSingleImage">
                 { isLoaded && (
                 <>
                     <div className="imageDisplay">
@@ -40,12 +44,35 @@ function SingleImage() {
                             title={image.title}
                             className={grow}
                             onClick={toggleGrow}/>
+                        {image.User.id === sessionUser.id?
+                            <div className="trashCan"
+                                onClick={() => setShowDelete(true)}>
+                                <i className="fas fa-trash-alt"></i>
+                            </div>
+                            : null}
                     </div>
                     <div className="imageDetails">
                         <h1>{image.User.username}</h1>
                         <h2>{image.title}</h2>
 
                     </div>
+                    {showDelete && (
+                        <>
+                            <div className="modalBackground">
+                                <div className="deleteForm">
+                                    <div className="topRowDelete">
+                                        <h3>Delete Photo</h3>
+                                        <div onClick={() => setShowDelete(false)}>X</div>
+                                    </div>
+                                    <div className="doYouWant">Do you want to permanently delete this photo?</div>
+                                    <div className="bottomRowDelete">
+                                        <div className="cancelDelete" onClick={() => setShowDelete(false)}>Cancel</div>
+                                        <div className="deleteDelete">Delete</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </>
                 )}
             </main>
