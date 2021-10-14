73fed8a2f3864bbe47a364d13f6d26be192a59b8
Jack Percival <jackpercival7@gmail.com>
2021-10-14 14:26:36 -0700

    Can create albums without content

--- frontend/src/components/UploadAlbum/uploadAlbum.js
+++ frontend/src/components/UploadAlbum/uploadAlbum.js
@@ -4,23 +4,29 @@ import UploadAlbumImageHolder from '../UploadAlbumImageHolder/uploadAlbumImageHo
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { getAllImages } from '../../store/images';
+import { addNewAlbum } from '../../store/albums';
 
 import './uploadAlbum.css'
 
 function UploadAlbum() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const images = useSelector(state => Object.values(state.images));
+    const sessionUser = useSelector(state => state.session.user);
 
     const [isLoaded, setIsLoaded] = useState(false);
     const [title, setTitle] = useState('');
     const [selectedImages, setSelectedImages] = useState(new Set());
+    const [errors, setErrors] = useState([]);
 
     useEffect(() => {
-        dispatch(getAllImages()).then(() => setIsLoaded(true));
-    }, [dispatch]);
+        if (!isLoaded) {
+            dispatch(getAllImages()).then(() => setIsLoaded(true));
+        }
+    }, [isLoaded, dispatch]);
 
     //Add or remove images to the selectedImages state
     const toggleSelectedImages = (imageId) => {
@@ -33,6 +39,39 @@ function UploadAlbum() {
         }
 
         setSelectedImages(currentImages);
+    }
+
+    //Check for no images and title length
+    //Then dispatch to create the album and return the album id
+    //Then for each image in the set, dispatch to create a row in album_contents
+    //Then redirect to the new album
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const errors = [];
+
+        if (selectedImages.size === 0) {
+            errors.push("You must select at least 1 image.")
+        }
+        if (title.length === 0 || title.length > 30) {
+            errors.push("Title must be between 1 and 30 characters.")
+        }
+
+        if (errors.length) {
+            setErrors(errors)
+            return;
+        }
+
+        const payload = {
+            title,
+            user_id: sessionUser.id
+        }
+
+        const newAlbum = await dispatch(addNewAlbum(payload));
+
+        if (newAlbum) {
+            history.push(`/albums/${newAlbum.id}`)
+        }
+
     }
 
     return (
@@ -53,10 +92,10 @@ function UploadAlbum() {
                         <div className="imageEditFormContainer">
                             <h3>Creating 1 Album:</h3>
                             <ul>
-                                {/* {validationUploadErrors.map(error => <li className="loginError" key={error}>{error}</li>)} */}
+                                {errors.map(error => <li className="loginError" key={error}>{error}</li>)}
                             </ul>
                             <div>
-                                <form className="imageEditForm" autoComplete="off">
+                                <form className="imageEditForm" autoComplete="off" onSubmit={handleSubmit}>
                                     <div>
                                         <label htmlFor="title">Album Title</label>
                                         <input id="title"
