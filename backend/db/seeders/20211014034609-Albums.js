9ad0c825398fcd32d8a770fe29d200a0d957d285
Jack Percival <jackpercival7@gmail.com>
2021-10-13 21:01:52 -0700

    Add album data model

---
+++ backend/db/seeders/20211014034609-Albums.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Albums', [
+     {
+     title: 'Barcelona',
+     user_id: 1
+    }
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Albums', null, {});
+  }
+};
